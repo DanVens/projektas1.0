e6d53b3d9fa8eebf6d7043b1724d80a7bc38eea5
DanVens <60598434+DanVens@users.noreply.github.com>
2023-05-31 12:11:39 +0300

    Create struktura.h

---
+++ struktura.h
@@ -0,0 +1,25 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include "bibliotekos.h"
+#include "struktura.h"
+struct studentas //struktūra, nusakanti studento duomenis
+{
+    string vardas, pavarde;
+    vector<int> paz;
+    int egz;
+    double vidurkis;
+    string metodas = "";
+};
+
+void pildymas(vector<studentas>& a);
+void rusiavimas(studentas A);
+bool palyginimas(studentas a, studentas b);
+void rusiavimasStudentu(vector<studentas>& A);
+void galutinisVid(studentas& A);
+void galutinisMed(studentas& A);
+void Spausdinimas(vector<studentas> A);
+void FailuGeneravimas(int & pazsk);
+void perrasymas(int pazsk);
+
+#endif  // FUNKCIJOS_H
