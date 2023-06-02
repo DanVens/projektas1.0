4c4c1a028800908adcd15d64cb0df113bc167919
DanVens <60598434+DanVens@users.noreply.github.com>
2023-06-02 13:07:16 +0300

    Create bibliotekos.h

---
+++ 0.5listai/bibliotekos.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cstdio>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <chrono>
+#include <time.h>
+#include <random>
+#include <list>
+using std::getline;
+using std::list;
+using std::vector;
+using std::swap;
+using std::fixed;
+using std::stoi;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::copy;
+using std::ifstream;
+using std::ofstream;
+
+
